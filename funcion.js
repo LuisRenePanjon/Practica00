7e1c6b64db70f8f6211d1a2fc0377703b99ed1bd
LuisRenePanjon <lucho@LAPTOP-1AESCG16>
2020-04-10 19:55:10 -0500

    funcion de paginacion Siguiente-Atras

--- funcion.js
+++ funcion.js
@@ -1,6 +1,9 @@
-function apiCall(){
-    var titulo=document.getElementById("cajaText").value;
+var pagina=1;
 
+function apiCall(){
+    
+    var titulo=document.getElementById("cajaText").value;
+    
     var detalles="";
     if(titulo == ""){
         detalles="<tr>" + "<td colspan='S'> Debe ingresar un titulo, por ejm: Batman </td>" + "</tr>";
@@ -15,7 +18,6 @@ function apiCall(){
         xmlhttp.onreadystatechange = function(){
             if(this.readyState == 4 && this.status ==200){
                 var datos = JSON.parse(this.responseText)
-
                 datos.Search.forEach(pelicula => {
                     detalles += "<tr>" +
                                 "<td><a href='#' onclick=\"buscarPeliculaPorId('" + pelicula.imdbID + "')\">Mas informacion</td>" +
@@ -23,17 +25,97 @@ function apiCall(){
                                 "<td>" + pelicula.Year + "</td>" +
                                 "<td>" + pelicula.Type + "</td>" +
                                 "<td><img src=" + pelicula.Poster + "></td>" +
-                                "</tr>";
+                                "</tr>" ;
+                                
                 });
+                detalles += "<input type='button' value='Anterior' onclick='paginas()' class='boton'>"+
+                            "<input type='button' value='Siguiente' onclick='paginas()' class='boton'>";
+                            
                 document.getElementById("informacion").innerHTML = detalles;
             }
         };
-        xmlhttp.open("GET","http://www.omdbapi.com/?apikey=adcd043d&s=" + titulo + "&plot=full", true);
+        xmlhttp.open("GET","http://www.omdbapi.com/?apikey=adcd043d&s=" + titulo + "&page="+pagina , true);
         xmlhttp.send();
     }
 
+}
 
+function paginas(){
+    pagina= pagina+1;
+    var titulo=document.getElementById("cajaText").value;
+    
+    var detalles="";
+    if(titulo == ""){
+        detalles="<tr>" + "<td colspan='S'> Debe ingresar un titulo, por ejm: Batman </td>" + "</tr>";
+        document.getElementById("informacion").innerHTML = detalles;
+    }else{
+        if(window.XMLHttpRequest){
+            xmlhttp = new XMLHttpRequest();
 
+        }else{
+            xmlhttp= new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status ==200){
+                var datos = JSON.parse(this.responseText)
+                datos.Search.forEach(pelicula => {
+                    detalles += "<tr>" +
+                                "<td><a href='#' onclick=\"buscarPeliculaPorId('" + pelicula.imdbID + "')\">Mas informacion</td>" +
+                                "<td>" + pelicula.Title + "</td>" +
+                                "<td>" + pelicula.Year + "</td>" +
+                                "<td>" + pelicula.Type + "</td>" +
+                                "<td><img src=" + pelicula.Poster + "></td>" +
+                                "</tr>" ;
+                                
+                });
+                detalles += "<input type='button' value='Anterior' onclick='paginas2()' class='boton'>"+
+                            "<input type='button' value='Siguiente' onclick='paginas()' class='boton'>";
+                
+                document.getElementById("informacion").innerHTML = detalles;
+            }
+        };
+        xmlhttp.open("GET","http://www.omdbapi.com/?apikey=adcd043d&s=" + titulo + "&page="+pagina , true);
+        xmlhttp.send();
+    }
+}
+
+function paginas2(){
+    pagina= pagina-1;
+    var titulo=document.getElementById("cajaText").value;
+    
+    var detalles="";
+    if(titulo == ""){
+        detalles="<tr>" + "<td colspan='S'> Debe ingresar un titulo, por ejm: Batman </td>" + "</tr>";
+        document.getElementById("informacion").innerHTML = detalles;
+    }else{
+        if(window.XMLHttpRequest){
+            xmlhttp = new XMLHttpRequest();
+
+        }else{
+            xmlhttp= new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status ==200){
+                var datos = JSON.parse(this.responseText)
+                datos.Search.forEach(pelicula => {
+                    detalles += "<tr>" +
+                                "<td><a href='#' onclick=\"buscarPeliculaPorId('" + pelicula.imdbID + "')\">Mas informacion</td>" +
+                                "<td>" + pelicula.Title + "</td>" +
+                                "<td>" + pelicula.Year + "</td>" +
+                                "<td>" + pelicula.Type + "</td>" +
+                                "<td><img src=" + pelicula.Poster + "></td>" +
+                                "</tr>" ;
+                                
+                });
+                detalles += "<input type='button' value='Anterior' onclick='paginas2()' class='boton'>"+
+                            "<input type='button' value='Siguiente' onclick='paginas()' class='boton'>";
+                
+                document.getElementById("informacion").innerHTML = detalles;
+            }
+        };
+        xmlhttp.open("GET","http://www.omdbapi.com/?apikey=adcd043d&s=" + titulo + "&page="+pagina , true);
+        xmlhttp.send();
+    }
 }
 
 
